2bd58618497edf9640b1d32ec0128bd550d8f4da
hira-neee <leehoanqnam@gmail.com>
2025-05-31 18:36:10 +0700

    Update script.js
    Signed-off-by: hira-neee <leehoanqnam@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function showMore() {
   document.getElementById("moreInfo").classList.toggle("hidden");
 }
+
+setTimeout(() => {
+  const bar = document.getElementById("notifyBar");
+  bar.style.transition = "top 0.5s, opacity 0.5s";
+  bar.style.top = "-50px";
+  bar.style.opacity = "0";
+}, 5000); // 5 giây sau biến mất
